6c0a31e36bc85cf160444f7979efd8da9a4dffad
mikeKaufman310 <mpk225@lehigh.edu>
2024-05-16 15:34:25 -0400

    game frontend flow is more defined

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 I am bored, so I am making this black jack web app
 
-todo:
-- figure out ide package things/bug
-- finish shuffle in deck
-- implement game logic
-- make rest endpoints for game logic
-- make js frontend logic
-- make web frontend
-- use ngrok or something to host both back and front ends
-- get money
 
 running it:
 backend => run "make", run "make run"
--- web/index.js
+++ web/index.js
@@ -1,25 +1,73 @@
 const backendUrl = 'http://localhost:4567';
-fetch(`${backendUrl}/hands`)
-.then(response => response.json())
-.then(data => {
-    const firstName = data.data[0].hand[0].name;
-    const firstValue = data.data[0].hand[0].value;
-    const firstSuitClub = data.data[0].hand[0].suit.club;
-    const firstSuitDiamond = data.data[0].hand[0].suit.diamond;
-    const firstSuitHeart = data.data[0].hand[0].suit.heart;
-    let suit = "";
-    if(firstSuitClub){
-        suit = "Club";
-    }else if(firstSuitDiamond){
-        suit = "Diamond";
-    }else if(firstSuitHeart){
-        suit = "Heart";
-    }else{
-        suit = "Spade";
+let numDealerCards = 0;
+let numPlayerCards = 0;
+
+console.log("Waiting for game to start");
+function gameStart(){
+    console.log("Starting game");
+    fetch(`${backendUrl}/hands`)
+    .then(response => response.json())
+    .then(data => {
+        numDealerCards = 2;
+        numPlayerCards = 2;
+        console.log("Dealing initial hands");
+        displayHands(data, numDealerCards, numPlayerCards);
+    })  
+    .catch(error => {
+        console.error("Error in response");
+    });
+}
+
+function playerHit(){
+    //player hit code
+    //if bust end game
+}
+
+function dealerTurn(){
+    //dealer's turn code
+    endGame();
+}
+
+function doubleDown(){
+    //double down code
+    dealerTurn();
+}
+
+function endGame(){
+    //end game code
+    //calculate scores and display output
+}
+
+
+function displayHands(data, numDealerCards, numPlayerCards){
+    let dealerCards = document.getElementById("dealerCards");
+    let dealerCardsStr = "";
+    let playerCards = document.getElementById("playerCards");
+    let playerCardsStr = "";
+    for(let i = 0; i < numDealerCards; i++){
+        dealerCardsStr += "Card " + (i+1) + ": " + data.data[0].hand[i].name + " of " + suitParse(data.data[0].hand[i].suit) + "<br>";
     }
-    document.write("Card: " + firstName + "<br>Suit: " + suit);
-    console.log(data.data[0]);
-})
-.catch(error => {
-    console.error("Error in response");
-})
+    for(let i = 0; i < numPlayerCards; i++){
+        playerCardsStr += "Card " + (i+1) + ": " + data.data[1].hand[i].name + " of " + suitParse(data.data[1].hand[i].suit) + "<br>";
+    }
+    dealerCards.innerHTML = dealerCardsStr;
+    playerCards.innerHTML = playerCardsStr;
+}
+
+function suitParse(suit){
+    let res = "";
+    if(suit.club){
+        res = "Club";
+    }else if(suit.diamond){
+        res = "Diamond";
+    }else if(suit.heart){
+        res = "Heart";
+    }else{
+        res = "Spade";
+    }
+    return res;
+}
+
+function initiateStart(bool){
+    bool = true;
+}
